f87642b397811ba7a7e0d00d0331ccfe4b4a568c
Rusty Eddy <rusty@rustyeddy.com>
2025-10-25 11:32:08 -0700

    Update main.go with new content

--- main.go
+++ main.go
@@ -2,28 +2,32 @@ package main
 
 import (
 	"flag"
-	"time"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/rustyeddy/devices"
-	"github.com/rustyeddy/otto"
+	ottoPkg "github.com/rustyeddy/otto"
 )
 
 var (
 	stationName = "gardener"
-	mock        = true
-	period      = 15 * time.Minute
+	mock        = false
 	useLocal    = false
-	mqttBroker  = ""
+	mqttBroker  = "test.mosquitto.org"
 )
 
 func init() {
 	flag.BoolVar(&mock, "mock", false, "mock gpio")
 	flag.BoolVar(&useLocal, "local", false, "use local messaging (no MQTT)")
-	flag.StringVar(&mqttBroker, "mqtt-broker", "", "MQTT broker address (default: test.mosquitto.org)")
+	flag.StringVar(&mqttBroker, "mqtt-broker", mqttBroker, "MQTT broker address (default: test.mosquitto.org)")
 }
 
 func main() {
 	flag.Parse()
+
+	log.Printf("starting %s (mock=%v, local=%v, mqtt=%q)", stationName, mock, useLocal, mqttBroker)
 
 	// Enable mocking in devices if mock flag is set
 	if mock {
@@ -31,15 +35,28 @@ func main() {
 	}
 
 	gardener := &Gardener{}
-	otto := &otto.OttO{
-		Name:       "gardener",
+	ottClient := &ottoPkg.OttO{
+		Name:       stationName,
 		Mock:       mock,
 		UseLocal:   useLocal,
 		MQTTBroker: mqttBroker,
 	}
-	gardener.OttO = otto
+	gardener.OttO = ottClient
 	gardener.Init()
-	gardener.Start()
+	if err := gardener.Start(); err != nil {
+		log.Printf("failed to start gardener: %v", err)
+	}
+
+	// Handle OS signals and call Stop() for graceful shutdown
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-signals
+		log.Printf("received signal %s, stopping gardener", sig)
+		gardener.Stop()
+	}()
+
 	<-gardener.Done()
 	gardener.Stop()
+	log.Println("gardener stopped")
 }
